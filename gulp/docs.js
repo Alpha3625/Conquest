e809a7af6bfdf7cc452ce589e766d2ece56c90b9
ArtyomXXX <ambartsumyanartyomxxx@gmail.com>
2023-09-29 00:01:00 +0300

    Ввёл корректировки в gulp

--- gulp/docs.js
+++ gulp/docs.js
@@ -1,19 +1,15 @@
 const gulp = require('gulp');
 
 // HTML
-const htmlclean = require('gulp-htmlclean');
 const fileInclude = require('gulp-file-include');
+const htmlclean = require('gulp-htmlclean');
+const webpHTML = require('gulp-webp-html');
 
 // SASS
 const sass = require('gulp-sass')(require('sass'));
 const sassGlob = require('gulp-sass-glob');
 const autoprefixer = require('gulp-autoprefixer');
 const csso = require('gulp-csso');
-const webpCSS = require('gulp-webp-css');
-
-// Images
-const webp = require('gulp-webp');
-const webpHTML = require('gulp-webp-html');
 
 const server = require('gulp-server-livereload');
 const clean = require('gulp-clean');
@@ -25,102 +21,98 @@ const webpack = require('webpack-stream');
 const babel = require('gulp-babel');
 const changed = require('gulp-changed');
 
-// Clean task
-gulp.task('clean:docs', function(done) {
-    if (fs.existsSync('./docs/')) {
-        return gulp.src('./docs/', {read: false})
-            .pipe(clean({force: true}));
-    }
+// Images
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
-    done();
+
+gulp.task('clean:docs', function (done) {
+	if (fs.existsSync('./docs/')) {
+		return gulp
+			.src('./docs/', { read: false })
+			.pipe(clean({ force: true }));
+	}
+	done();
 });
 
-// File include settings task
-const fileIncludeSettings = {
-    prefix: '@@',
-    basepath: '@file',
+const fileIncludeSetting = {
+	prefix: '@@',
+	basepath: '@file',
 };
 
-// Plumber notify task
 const plumberNotify = (title) => {
-    return {
-        errorHandler: notify.onError({
-            title: title,
-            message: 'Error <%= error.message %>',
-            sound: false,
-        }),
-    };
-}
+	return {
+		errorHandler: notify.onError({
+			title: title,
+			message: 'Error <%= error.message %>',
+			sound: false,
+		}),
+	};
+};
 
-// HTML task
-gulp.task('html:docs', function() {
-    return (
-        gulp.src(['./src/html/**/*.html', '!./src/html/blocks/*.html'])
-            .pipe(changed('./docs/'))
-            .pipe(plumber(plumberNotify('HTML')))
-            .pipe(fileInclude(fileIncludeSettings))
-            .pipe(webpHTML())
-            .pipe(htmlclean())
-            .pipe(gulp.dest('./docs/'))
-    );
+gulp.task('html:docs', function () {
+	return gulp
+		.src(['./src/html/**/*.html', '!./src/html/blocks/**/*.html'])
+		.pipe(changed('./docs/'))
+		.pipe(plumber(plumberNotify('HTML')))
+		.pipe(fileInclude(fileIncludeSetting))
+		.pipe(webpHTML())
+		.pipe(htmlclean())
+		.pipe(gulp.dest('./docs/'));
 });
 
-// SCSS task
-gulp.task('scss:docs', function() {
-    return (
-        gulp.src('./src/scss/*.scss')
-            .pipe(changed('./docs/css/'))
-            .pipe(plumber(plumberNotify('SCSS')))
-            .pipe(sourceMaps.init())
-            .pipe(autoprefixer()
-            .pipe(webpCSS())
-            .pipe(sassGlob())
-            .pipe(sass()))
-            .pipe(csso())
-            .pipe(sourceMaps.write())
-            .pipe(gulp.dest('./docs/css/'))
-    );
+gulp.task('sass:docs', function () {
+	return gulp
+		.src('./src/scss/*.scss')
+		.pipe(changed('./docs/css/'))
+		.pipe(plumber(plumberNotify('SCSS')))
+		.pipe(sourceMaps.init())
+		.pipe(autoprefixer())
+		.pipe(sassGlob())
+		.pipe(sass())
+		.pipe(csso())
+		.pipe(sourceMaps.write())
+		.pipe(gulp.dest('./docs/css/'));
 });
 
-// Images task
-gulp.task('images:docs', function() {
-    return (
-        gulp.src('./src/images/**/*')
-            .pipe(changed('./docs/images/'))
-            .pipe(webp())
-            .pipe(gulp.dest('./docs/images/'))
-
-            .pipe(gulp.src('./src/images/**/*'))
-            .pipe(changed('./docs/images/'))
-            .pipe(gulp.dest('./docs/images/'))
-    );
+gulp.task('images:docs', function () {
+	return gulp
+		.src('./src/img/**/*')
+		.pipe(changed('./docs/img/'))
+		.pipe(webp())
+		.pipe(gulp.dest('./docs/img/'))
+		.pipe(gulp.src('./src/img/**/*'))
+		.pipe(changed('./docs/img/'))
+		.pipe(gulp.dest('./docs/img/'));
 });
 
-// Icons task
-gulp.task('icons:docs', function() {
-    return gulp.src('./src/icons/*.svg')
-        .pipe(gulp.dest('./docs/icons/'));
+
+gulp.task('icons:docs', function () {
+	return gulp
+		.src('./src/icons/**/*')
+		.pipe(changed('./docs/icons/'))
+		.pipe(webp())
+		.pipe(gulp.dest('./docs/icons/'))
+		.pipe(gulp.src('./src/icons/**/*'))
+		.pipe(changed('./docs/icons/'))
+		.pipe(gulp.dest('./docs/icons/'));
 });
 
-// JS task
-gulp.task('js:docs', function() {
-    return (
-        gulp.src('./src/js/*.js')
-            .pipe(changed('./docs/js'))
-            .pipe(plumber(plumberNotify('JS')))
-            .pipe(babel())
-            .pipe(webpack(require('../webpack.config.js')))
-            .pipe(gulp.dest('./docs/js/'))
-    );
+gulp.task('js:docs', function () {
+	return gulp
+		.src('./src/js/*.js')
+		.pipe(changed('./docs/js/'))
+		.pipe(plumber(plumberNotify('JS')))
+		.pipe(babel())
+		.pipe(webpack(require('./../webpack.config.js')))
+		.pipe(gulp.dest('./docs/js/'));
 });
 
-// Server options task
 const serverOptions = {
-    livereload: true,
-    open: true,
-}
+	livereload: true,
+	open: true,
+};
 
-// Server task
-gulp.task('server:docs', function() {
-    return gulp.src('./docs/').pipe(server(serverOptions));
+gulp.task('server:docs', function () {
+	return gulp.src('./docs/').pipe(server(serverOptions));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,20 @@ const gulp = require('gulp');
 require('./gulp/dev.js');
 require('./gulp/docs.js');
 
-gulp.task('default', gulp.series(
-    'clean:dev',
-    gulp.parallel('html:dev', 'scss:dev', 'images:dev', 'icons:dev', 'js:dev'),
-    gulp.parallel('server:dev', 'watch:dev')
-));
+gulp.task(
+	'default',
+	gulp.series(
+		'clean:dev',
+		gulp.parallel('html:dev', 'sass:dev', 'images:dev', 'icons:dev', 'js:dev'),
+		gulp.parallel('server:dev', 'watch:dev')
+	)
+);
 
-gulp.task('docs', gulp.series(
-    'clean:docs',
-    gulp.parallel('html:docs', 'scss:docs', 'images:docs', 'icons:docs', 'js:docs'),
-    gulp.parallel('server:docs')
-));
+gulp.task(
+	'docs',
+	gulp.series(
+		'clean:docs',
+		gulp.parallel('html:docs', 'sass:docs', 'images:docs', 'icons:docs', 'js:docs'),
+		gulp.parallel('server:docs')
+	)
+);
